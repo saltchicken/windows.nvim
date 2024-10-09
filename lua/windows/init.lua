fe34d0d1ceb8dc699770c6114910e9cc50013f4e
saltchicken <john.eicher89@gmail.com>
2024-10-09 14:02:19 -0700

    Create main class

--- lua/windows/init.lua
+++ lua/windows/init.lua
@@ -1,4 +1,27 @@
-local windows = {}
+local Windows = {}
+Windows.__index = Windows
 
-print("INIT from windows")
-return windows
+function Windows:new()
+	local obj = setmetatable({}, Windows)
+	obj.active_windows = {}
+	return obj
+end
+
+function Windows:floating_window(width, height, col, row, footer)
+	local buf = vim.api.nvim_create_buf(false, true)
+
+	local win_opts = {
+		relative = "editor",
+		width = width,
+		height = height,
+		col = col,
+		row = row,
+		style = "minimal",
+		border = "single",
+		footer = footer,
+	}
+	local win = vim.api.nvim_open_win(buf, true, win_opts)
+	table.insert(self.active_windows, { win, buf })
+end
+
+return Windows
